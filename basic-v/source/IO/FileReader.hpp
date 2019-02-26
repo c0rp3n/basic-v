fc7562c7055c2390790df345683fdfb52682972d
c0rp3n's Den <ojhitchcock@gmail.com>
2019-02-26 17:39:09 +0000

    Add IO File Reader
    The File Reader uses memory mapping minimize the amount of bottle necking that can appear from the standard library.
    Also added a simple command line argument parser though will need more input safety.

---
+++ basic-v/source/IO/FileReader.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "../../../Libs/mio/single_include/mio/mio.hpp"
+
+namespace bv
+{
+	namespace IO
+	{
+		static class FileReader
+		{
+		public:
+			static std::vector<std::string> ReadLines(std::string filepath)
+			{
+				std::vector<std::string> lines;
+				std::string data;
+				{
+					mio::mmap_source mmap(filepath);
+					data = std::string(mmap.data);
+				}
+
+				std::vector<uint64_t> lineLengths;
+				{
+					uint64_t lineLength = 0;
+					for (const char& value : data)
+					{
+						if (value == '\n')
+						{
+							lineLengths.push_back(lineLength);
+							lineLength = 0;
+						}
+						else
+						{
+							lineLength++;
+						}
+					}
+				}
+
+				{
+					uint64_t pointer = 0;
+					for (uint64_t& i : lineLengths)
+					{
+						const std::string line = data.substr(pointer, i);
+						lines.push_back(line);
+
+						pointer += i;
+					}
+				}
+
+				lineLengths.clear();
+
+				return lines;
+			}
+		};
+	}
+}
