b4d10504bd3e5675f9c5bcf0bb898cda90b70c52
c0rp3n's Den <ojhitchcock@gmail.com>
2019-02-26 18:01:27 +0000

    Fix indentation

--- basic-v/source/IO/FileReader.hpp
+++ basic-v/source/IO/FileReader.hpp
@@ -7,52 +7,52 @@
 
 namespace bv
 {
-	namespace IO
-	{
-		static class FileReader
-		{
-		public:
-			static std::vector<std::string> ReadLines(std::string filepath)
-			{
-				std::vector<std::string> lines;
-				std::string data;
-				{
-					mio::mmap_source mmap(filepath);
-					data = std::string(mmap.data);
-				}
+    namespace IO
+    {
+        static class FileReader
+        {
+        public:
+            static std::vector<std::string> ReadLines(std::string filepath)
+            {
+                std::vector<std::string> lines;
+                std::string data;
+                {
+                    mio::mmap_source mmap(filepath);
+                    data = std::string(mmap.data);
+                }
 
-				std::vector<uint64_t> lineLengths;
-				{
-					uint64_t lineLength = 0;
-					for (const char& value : data)
-					{
-						if (value == '\n')
-						{
-							lineLengths.push_back(lineLength);
-							lineLength = 0;
-						}
-						else
-						{
-							lineLength++;
-						}
-					}
-				}
+                std::vector<uint64_t> lineLengths;
+                {
+                    uint64_t lineLength = 0;
+                    for (const char& value : data)
+                    {
+                        if (value == '\n')
+                        {
+                            lineLengths.push_back(lineLength);
+                            lineLength = 0;
+                        }
+                        else
+                        {
+                            lineLength++;
+                        }
+                    }
+                }
 
-				{
-					uint64_t pointer = 0;
-					for (uint64_t& i : lineLengths)
-					{
-						const std::string line = data.substr(pointer, i);
-						lines.push_back(line);
+                {
+                    uint64_t pointer = 0;
+                    for (uint64_t& i : lineLengths)
+                    {
+                        const std::string line = data.substr(pointer, i);
+                        lines.push_back(line);
 
-						pointer += i;
-					}
-				}
+                        pointer += i;
+                    }
+                }
 
-				lineLengths.clear();
+                lineLengths.clear();
 
-				return lines;
-			}
-		};
-	}
+                return lines;
+            }
+        };
+    }
 }
--- basic-v/source/lexer.cpp
+++ basic-v/source/lexer.cpp
@@ -8,29 +8,29 @@
 
 int main(int argc, char* argv[])
 {
-	std::string filepath;
-	for (int i = 1; i < argc; i++)
-	{
-		if (argv[i][0] == 0)
-		{
-			continue;
-		}
+    std::string filepath;
+    for (int i = 1; i < argc; i++)
+    {
+        if (argv[i][0] == 0)
+        {
+            continue;
+        }
 
-		if (argv[i][0] == '-' && argv[i][1] != '-')
-		{
-			if (argv[i][1] == 'h')
-			{
-				// help text stuff here
-			}
-			else if (argv[i][1] == 'p')
-			{
-				if (i + 1 < argc)
-				{
-					filepath = argv[++i];
-				}
-			}
-		}
-	}
+        if (argv[i][0] == '-' && argv[i][1] != '-')
+        {
+            if (argv[i][1] == 'h')
+            {
+                // help text stuff here
+            }
+            else if (argv[i][1] == 'p')
+            {
+                if (i + 1 < argc)
+                {
+                    filepath = argv[++i];
+                }
+            }
+        }
+    }
 
-	std::vector<std::string> lines = bv::IO::FileReader::ReadLines(filepath);
+    std::vector<std::string> lines = bv::IO::FileReader::ReadLines(filepath);
 }
