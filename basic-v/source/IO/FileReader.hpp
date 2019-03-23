a72150c8d729f6171a74123f37512fa00981e38b
samconran <samconran7@gmail.com>
2019-03-23 11:53:52 +0000

    Replaces tabs with spaces

--- basic-v/source/IO/FileReader.hpp
+++ basic-v/source/IO/FileReader.hpp
@@ -6,41 +6,41 @@
 
 namespace bv
 {
-	namespace IO
-	{
-		static class FileReader
-		{
-		public:
-			/// <summary>
-			/// Parses all of the lines in a file.
-			/// </summary>
-			/// <param name="filepath">Path to the file including name and extension.</param>
-			/// <returns>A vector of each line in the file.</returns>
-			static std::vector<std::string> ReadLines(std::string filepath)
-			{
-				std::vector<std::string> lines;
-				// Open the File
-				std::ifstream in(filepath.c_str());
+    namespace IO
+    {
+        static class FileReader
+        {
+        public:
+            /// <summary>
+            /// Parses all of the lines in a file.
+            /// </summary>
+            /// <param name="filepath">Path to the file including name and extension.</param>
+            /// <returns>A vector of each line in the file.</returns>
+            static std::vector<std::string> ReadLines(std::string filepath)
+            {
+                std::vector<std::string> lines;
+                // Open the File
+                std::ifstream in(filepath.c_str());
 
-				// Check if object is valid
-				if (!in)
-				{
-					std::cerr << "Cannot open the File : " << filepath << std::endl;
-				}
+                // Check if object is valid
+                if (!in)
+                {
+                    std::cerr << "Cannot open the File : " << filepath << std::endl;
+                }
 
-				std::string str;
-				// Read the next line from File untill it reaches the end.
-				while (std::getline(in, str))
-				{
-					// Line contains string of length > 0 then save it in vector
-					if (str.size() > 0)
-						lines.push_back(str);
-				}
-				//Close The File
-				in.close();
+                std::string str;
+                // Read the next line from File untill it reaches the end.
+                while (std::getline(in, str))
+                {
+                    // Line contains string of length > 0 then save it in vector
+                    if (str.size() > 0)
+                        lines.push_back(str);
+                }
+                //Close The File
+                in.close();
 
-				return lines;
-			}
-		};
-	}
+                return lines;
+            }
+        };
+    }
 }
--- basic-v/source/lexer.cpp
+++ basic-v/source/lexer.cpp
@@ -12,305 +12,305 @@ std::vector<bv::Token> tokens;
 
 void generateTokens(std::string line, int lineNumber)
 {
-	iter = line.begin();
-	char character;
+    iter = line.begin();
+    char character;
 
-	while (iter < line.end())
-	{
-		character = *iter;
-		int position = iter - line.begin();
-		iter++;
+    while (iter < line.end())
+    {
+        character = *iter;
+        int position = iter - line.begin();
+        iter++;
 
-		//If a keywors or an identifier...
-		if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z') || (character == '_')) {
-			std::string value(1, character);
-			while (iter != line.end() && ((*iter >= 'a' && *iter <= 'z') || (*iter >= 'A' && *iter <= 'Z') || (*iter >= '0' && *iter <= '9') || (*iter == '_'))) {
-				value.append(1, *iter++);
-			}
+        //If a keywors or an identifier...
+        if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z') || (character == '_')) {
+            std::string value(1, character);
+            while (iter != line.end() && ((*iter >= 'a' && *iter <= 'z') || (*iter >= 'A' && *iter <= 'Z') || (*iter >= '0' && *iter <= '9') || (*iter == '_'))) {
+                value.append(1, *iter++);
+            }
 
-			// Look up whether or not the value matched predefined keywords.
-			// If so, add the keyword to the token vector
-			// If not, it is a custom identifier (see else)
-			if (value == "false")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::False));
-			}
-			else if (value == "true")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::True));
-			}
-			else if (value == "if")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::If));
-			}
-			else if (value == "then")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Then));
-			}
-			else if (value == "case")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Case));
-			}
-			else if (value == "of")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Of));
-			}
-			else if (value == "when")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::When));
-			}
-			else if (value == "otherwise")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Otherwise));
-			}
-			else if (value == "for")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::For));
-			}
-			else if (value == "to")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::To));
-			}
-			else if (value == "next")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Next));
-			}
-			else if (value == "repeat")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Repeat));
-			}
-			else if (value == "until")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Until));
-			}
-			else if (value == "while")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::While));
-			}
-			else if (value == "endwhile")
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::EndWhile));
-			}
-			else if (value == "end")
-			{
-				//If the keyword 'end' matches, look ahead another keyword to see if we need to match 'end if' or 'end case'
+            // Look up whether or not the value matched predefined keywords.
+            // If so, add the keyword to the token vector
+            // If not, it is a custom identifier (see else)
+            if (value == "false")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::False));
+            }
+            else if (value == "true")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::True));
+            }
+            else if (value == "if")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::If));
+            }
+            else if (value == "then")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Then));
+            }
+            else if (value == "case")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Case));
+            }
+            else if (value == "of")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Of));
+            }
+            else if (value == "when")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::When));
+            }
+            else if (value == "otherwise")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Otherwise));
+            }
+            else if (value == "for")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::For));
+            }
+            else if (value == "to")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::To));
+            }
+            else if (value == "next")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Next));
+            }
+            else if (value == "repeat")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Repeat));
+            }
+            else if (value == "until")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Until));
+            }
+            else if (value == "while")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::While));
+            }
+            else if (value == "endwhile")
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::EndWhile));
+            }
+            else if (value == "end")
+            {
+                //If the keyword 'end' matches, look ahead another keyword to see if we need to match 'end if' or 'end case'
 
-				std::string::iterator tempIter = iter;
-				std::string value2 = "";
-				if (*tempIter++ == ' ')
-				{
-					while (tempIter != line.end() && ((*tempIter >= 'a' && *tempIter <= 'z') || (*tempIter >= 'A' && *tempIter <= 'Z'))) {
-						value2.append(1, *tempIter++);
-					}
-				}
+                std::string::iterator tempIter = iter;
+                std::string value2 = "";
+                if (*tempIter++ == ' ')
+                {
+                    while (tempIter != line.end() && ((*tempIter >= 'a' && *tempIter <= 'z') || (*tempIter >= 'A' && *tempIter <= 'Z'))) {
+                        value2.append(1, *tempIter++);
+                    }
+                }
 
-				if (value2 == "if")
-				{
-					iter = tempIter;
-					tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::EndIf));
-				}
-				else if (value2 == "case")
-				{
-					iter = tempIter;
-					tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::EndCase));
-				}
-				else
-				{
-					tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::End));
-				}
-			}
-			else
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Identifier, value));
-			}
+                if (value2 == "if")
+                {
+                    iter = tempIter;
+                    tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::EndIf));
+                }
+                else if (value2 == "case")
+                {
+                    iter = tempIter;
+                    tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::EndCase));
+                }
+                else
+                {
+                    tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::End));
+                }
+            }
+            else
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Identifier, value));
+            }
 
 
-		}
+        }
 
-		//If an integer or a real number...
-		else if ((character >= '0' && character <= '9') || (character == '-' && *iter >= '0' && *iter <= '9'))
-		{
-			std::string value(1, character);
+        //If an integer or a real number...
+        else if ((character >= '0' && character <= '9') || (character == '-' && *iter >= '0' && *iter <= '9'))
+        {
+            std::string value(1, character);
 
-			while (iter != line.end() && *iter >= '0' && *iter <= '9' || (iter != line.end() && *iter == '.' && *(iter + 1) >= '0' && *(iter + 1) <= '9'))
-			{
-				value.append(1, *iter++);
-			}
+            while (iter != line.end() && *iter >= '0' && *iter <= '9' || (iter != line.end() && *iter == '.' && *(iter + 1) >= '0' && *(iter + 1) <= '9'))
+            {
+                value.append(1, *iter++);
+            }
 
-			//If the number contains a '.', it's a real. Otherwise, an integer.
-			if (value.find('.') != std::string::npos)
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Real, value));
-			}
-			else
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Integer, value));
-			}
-		}
+            //If the number contains a '.', it's a real. Otherwise, an integer.
+            if (value.find('.') != std::string::npos)
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Real, value));
+            }
+            else
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Integer, value));
+            }
+        }
 
-		//If a string literal...
-		else if (character == '"')
-		{
-			std::string value(1, *iter++);
-			while (iter != line.end() && *iter != '"') {
-				value.append(1, *iter++);
-			}
-			iter++;
-			tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::String, value));
-		}
+        //If a string literal...
+        else if (character == '"')
+        {
+            std::string value(1, *iter++);
+            while (iter != line.end() && *iter != '"') {
+                value.append(1, *iter++);
+            }
+            iter++;
+            tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::String, value));
+        }
 
-		//If '=' or '=='...
-		else if (character == '=')
-		{
-			if (iter != line.end() && *iter++ == '=')
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Equal));
-			}
-			else
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Assign));
-			}
-		}
+        //If '=' or '=='...
+        else if (character == '=')
+        {
+            if (iter != line.end() && *iter++ == '=')
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Equal));
+            }
+            else
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Assign));
+            }
+        }
 
-		//If '+'...
-		else if (character == '+')
-		{
-			tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Addition));
-		}
+        //If '+'...
+        else if (character == '+')
+        {
+            tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Addition));
+        }
 
-		//If '-'...
-		else if (character == '-')
-		{
-			tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Subtraction));
-		}
+        //If '-'...
+        else if (character == '-')
+        {
+            tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Subtraction));
+        }
 
-		//If '*'...
-		else if (character == '*')
-		{
-			tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Multiply));
-		}
+        //If '*'...
+        else if (character == '*')
+        {
+            tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Multiply));
+        }
 
-		//If '/'...
-		else if (character == '/')
-		{
-			tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Divide));
-		}
+        //If '/'...
+        else if (character == '/')
+        {
+            tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Divide));
+        }
 
-		//If '<', '<>', or '<='...
-		else if (character == '<')
-		{
-			if (iter != line.end() && *iter == '>')
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::NotEqual));
-			}
-			else if (iter != line.end() && *iter == '=')
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::LessThanOrEqual));
-			}
-			else
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::LessThan));
-			}
-			iter++;
-		}
+        //If '<', '<>', or '<='...
+        else if (character == '<')
+        {
+            if (iter != line.end() && *iter == '>')
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::NotEqual));
+            }
+            else if (iter != line.end() && *iter == '=')
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::LessThanOrEqual));
+            }
+            else
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::LessThan));
+            }
+            iter++;
+        }
 
-		//If '>' or '>='
-		else if (character == '>')
-		{
-			if (iter != line.end() && *iter++ == '=')
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::GreaterThanOrEqual));
-			}
-			else
-			{
-				tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::GreaterThan));
-			}
-		}
+        //If '>' or '>='
+        else if (character == '>')
+        {
+            if (iter != line.end() && *iter++ == '=')
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::GreaterThanOrEqual));
+            }
+            else
+            {
+                tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::GreaterThan));
+            }
+        }
 
-		//If '!'
-		else if (character == '!')
-		{
-			tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Not));
-		}
+        //If '!'
+        else if (character == '!')
+        {
+            tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Not));
+        }
 
-		//If '('...
-		else if (character == '(')
-		{
-			tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::OpenBracket));
-		}
+        //If '('...
+        else if (character == '(')
+        {
+            tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::OpenBracket));
+        }
 
-		//If ')'...
-		else if (character == ')')
-		{
-			tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::CloseBracket));
-		}
+        //If ')'...
+        else if (character == ')')
+        {
+            tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::CloseBracket));
+        }
 
-		//If ':'...
-		else if (character == ':')
-		{
-			tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Colon));
-		}
-	}
+        //If ':'...
+        else if (character == ':')
+        {
+            tokens.push_back(bv::Token(lineNumber, position, bv::Lexeme::Colon));
+        }
+    }
 }
 
 int main(int argc, char* argv[])
 {
-	std::string filepath;
-	for (int i = 1; i < argc; i++)
-	{
-		if (argv[i][0] == 0)
-		{
-			continue;
-		}
+    std::string filepath;
+    for (int i = 1; i < argc; i++)
+    {
+        if (argv[i][0] == 0)
+        {
+            continue;
+        }
 
-		if (argv[i][0] == '-' && argv[i][1] != '-')
-		{
-			if (argv[i][1] == 'h')
-			{
-				// help text stuff here
-			}
-			else if (argv[i][1] == 'p')
-			{
-				if (i + 1 < argc)
-				{
-					filepath = argv[++i];
-				}
-			}
-		}
-	}
+        if (argv[i][0] == '-' && argv[i][1] != '-')
+        {
+            if (argv[i][1] == 'h')
+            {
+                // help text stuff here
+            }
+            else if (argv[i][1] == 'p')
+            {
+                if (i + 1 < argc)
+                {
+                    filepath = argv[++i];
+                }
+            }
+        }
+    }
 
-	std::vector<std::string> lines = bv::IO::FileReader::ReadLines(filepath);
-	if (lines.size() < 1)
-	{
-		return 0;
-	}
+    std::vector<std::string> lines = bv::IO::FileReader::ReadLines(filepath);
+    if (lines.size() < 1)
+    {
+        return 0;
+    }
 
-	int lineNumber = 0;
-	for (std::string line : lines)
-	{
-		lineNumber++;
-		generateTokens(line, lineNumber);
-	}
+    int lineNumber = 0;
+    for (std::string line : lines)
+    {
+        lineNumber++;
+        generateTokens(line, lineNumber);
+    }
 
-	/*
-	<===== Commenting out for now, just for simplicity of testing the rules =====>
+    /*
+    <===== Commenting out for now, just for simplicity of testing the rules =====>
 
-	int threadCount = std::thread::hardware_concurrency();
-	int linesPerThread = lines.size() / threadCount;
-	std::vector<std::thread> threads;
-	threads.reserve(threadCount);
-	for (int i = 0; i < threadCount; i++)
-	{
-		threads.push_back(std::thread());
-	}
+    int threadCount = std::thread::hardware_concurrency();
+    int linesPerThread = lines.size() / threadCount;
+    std::vector<std::thread> threads;
+    threads.reserve(threadCount);
+    for (int i = 0; i < threadCount; i++)
+    {
+        threads.push_back(std::thread());
+    }
 
-	for (std::thread &t : threads)
-	{
-		t.join();
-	}
+    for (std::thread &t : threads)
+    {
+        t.join();
+    }
 
-	*/
+    */
 
 
-	return 0;
+    return 0;
 }
