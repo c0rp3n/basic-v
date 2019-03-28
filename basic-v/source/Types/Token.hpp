d4cb5869ec2e9e504f6acfcfe5414ab817d4a001
c0rp3n's Den <ojhitchcock@gmail.com>
2019-03-28 18:21:49 +0000

    Base work

--- basic-v/source/Types/Token.hpp
+++ basic-v/source/Types/Token.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <cstdint>
+#include <iostream>
 #include <string>
+
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
 
 #include "Lexeme.hpp"
 
@@ -24,6 +28,61 @@ namespace bv
             this->position = position;
             this->token = token;
             this->value = value;
+        }
+
+        bool Parse(std::string jsonpath, std::vector<Token>* tokens)
+        {
+            rapidjson::Document d;
+            std::ifstream ifs(jsonpath);
+            d.ParseStream(ifs);
+            
+            if (d["tokens"].IsArray())
+            {
+                for (auto& v : d.GetArray())
+                {
+                    tokens->push_back(Token
+                    (
+                        v["line"].GetUint64(),
+                        v["position"].GetUint64(),
+                        (Lexeme)(v["token"].GetUint64()),
+                        std::string(v["value"].GetString())
+                    ));
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        bool Serialise(std::string jsonpath, std::vector<Token>* tokens)
+        {
+            rapidjson::Document d;
+            d.SetObject();
+            rapidjson::Document::AllocatorType& allocator = d.GetAllocator();
+
+            rapidjson::Value array(rapidjson::kArrayType);
+            array.Reserve(sizeof(uint64_t) * 4 * tokens->size(), allocator);
+            for (Token& t : *tokens)
+            {
+                rapidjson::Value token(rapidjson::kObjectType);
+                token.AddMember("line", t.line, allocator);
+                token.AddMember("position", t.position, allocator);
+                token.AddMember("token", t.token, allocator);
+                token.AddMember("value", t.value, allocator);
+
+                array.PushBack(token, allocator);
+            }
+
+            d.AddMember("tokens", array, allocator);
+            
+            std::ofstream ofs(jsonpath);
+            rapidjson::Writer<std::ostream> w(ofs);
+            d.Accept(w);
+
+            ofs.close();
         }
     };
 }
