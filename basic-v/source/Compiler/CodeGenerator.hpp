b122e10333a72fe8faf9ef81d95583c216f41075
c0rp3n <ojhitchcock@gmail.com>
2019-03-28 12:12:10 +0000

    Stash

--- basic-v/source/Compiler/CodeGenerator.hpp
+++ basic-v/source/Compiler/CodeGenerator.hpp
@@ -1,12 +1,21 @@
 #pragma once
 
+#include <vector>
+
+#include "../Types/AsTree.hpp"
+#include "../Types/Lexeme.hpp"
+#include "../Types/Token.hpp"
+
 namespace bv::Compiler
 {
 	class CodeGenerator
 	{
-		static void Parse()
+		static void Parse(std::vector<AsTree>* tree)
 		{
-
+			while (true)
+			{
+				
+			}
 		}
 	};
 }
