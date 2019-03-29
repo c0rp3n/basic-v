2d4eb2d0bc4f16ddb738f4e6e49518b43d5b5cc8
Oliver Hitchcock <ojhitchcock@gmail.com>
2019-03-29 15:42:50 +0000

    [Parser] Overhaul Tree and add Data, Var and Function entries (#15)
    * Fix tree branching
    
    Tree flow should go to the right and branches go to the left.
    
    * Change to tree and node structure
    
    * Add Data and Function entries
    
    * Add mutability
    
    * Add expression to tree and rename left and right
    
    * Add expression type only has lhs and rhs
    
    * Rename to PTree

---
+++ basic-v/source/Types/PTree.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <vector>
+
+#include "Token.hpp"
+
+namespace bv
+{
+    struct PTree
+    {
+	public:
+		struct Node;
+        struct Expression;
+
+		std::vector<Node> nodes;
+        std::vector<Expression> expressions;
+
+        std::vector<Node*> trees;
+
+		PTree()
+		{
+		}
+
+		struct Node
+		{
+            Node* fork;
+            Expression* expression;
+			Node* flow;
+
+			std::vector<Token> tokens;
+
+			Node()
+			{
+				this->fork = nullptr;
+                this->expression = nullptr;
+				this->flow = nullptr;
+			}
+
+			Node(std::vector<Token> tokens, Node* flow, Node* fork, Expression* expression)
+			{
+				this->tokens = tokens;
+				this->fork = fork;
+                this->expression = expression;
+				this->flow = flow;
+			}
+		};
+
+        struct Expression
+        {
+            Expression* lhs;
+            Expression* rhs;
+
+            std::vector<Token> tokens;
+
+            Expression()
+            {
+                this->lhs = nullptr;
+                this->rhs = nullptr;
+            }
+
+            Expression(std::vector<Token> tokens, Expression* lhs, Expression* rhs)
+            {
+                this->lhs = lhs;
+                this->rhs = rhs;
+            }
+        };
+    };
+}
