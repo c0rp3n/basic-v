904bf8cbbce7be45a30386f4d198349195c7a12e
Oliver Hitchcock <ojhitchcock@gmail.com>
2019-05-01 13:44:34 +0100

    [IO][Parser] Read Tokens JSON (#22)
    Added support in the parser for the tokens json file.

--- basic-v/source/parser.cpp
+++ basic-v/source/parser.cpp
@@ -7,8 +7,37 @@
 
 int main(int argc, char* argv[])
 {
+	std::string filepath;
+	for (int i = 1; i < argc; i++)
+	{
+		if (argv[i][0] == 0)
+		{
+			continue;
+		}
+
+		if (argv[i][0] == '-' && argv[i][1] != '-')
+		{
+			if (argv[i][1] == 'h')
+			{
+				// todo: help text stuff here
+			}
+			else if (argv[i][1] == 'p')
+			{
+				if (i + 1 < argc)
+				{
+					filepath = argv[++i];
+				}
+			}
+		}
+	}
+
+	if (filepath == "")
+	{
+		return 0;
+	}
 
     std::vector<bv::Token> tokens;
+	bv::Token::Parse(filepath , &tokens);
 
     bv::Parser::RecursiveDescent parser;
     parser.Parse(&tokens);
