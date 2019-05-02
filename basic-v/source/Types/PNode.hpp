a3a704a322da70e81434279408a6c870d376a01d
c0rp3n <ojhitchcock@gmail.com>
2019-05-02 11:35:42 +0100

    Untabify

--- basic-v/source/Types/PNode.hpp
+++ basic-v/source/Types/PNode.hpp
@@ -5,71 +5,71 @@
 
 namespace bv
 {
-	struct PNode
-	{
-	public:
-		std::vector<size_t> branches;
-		size_t datum;
+    struct PNode
+    {
+    public:
+        std::vector<size_t> branches;
+        size_t datum;
 
-		PNode()
-		{
-		}
+        PNode()
+        {
+        }
 
-		PNode(size_t datum)
-		{
-			this->datum = datum;
-		}
+        PNode(size_t datum)
+        {
+            this->datum = datum;
+        }
 
-		static bool Parse(std::string jsonpath, std::vector<PNode>* tree)
-		{
-			std::ifstream ifs(jsonpath);
-			if (ifs.bad())
-			{
-				return false;
-			}
+        static bool Parse(std::string jsonpath, std::vector<PNode>* tree)
+        {
+            std::ifstream ifs(jsonpath);
+            if (ifs.bad())
+            {
+                return false;
+            }
 
-			std::string data;
-			data.assign((std::istreambuf_iterator<char>(ifs)),
-				(std::istreambuf_iterator<char>()));
+            std::string data;
+            data.assign((std::istreambuf_iterator<char>(ifs)),
+                (std::istreambuf_iterator<char>()));
 
-			nlohmann::json json = nlohmann::json::parse(data);
-			if (json["nodes"].is_array())
-			{
-				for (auto& e : json["nodes"].items())
-				{
-					auto& value = e.value();
+            nlohmann::json json = nlohmann::json::parse(data);
+            if (json["nodes"].is_array())
+            {
+                for (auto& e : json["nodes"].items())
+                {
+                    auto& value = e.value();
 
-					PNode node(value["datum"].get<size_t>());
-					for (auto& b : value["branches"].items())
-					{
-						node.branches.push_back(b.value().get<size_t>());
-					}
-					tree->push_back(node);
-				}
-			}
-			else
-			{
-				return false;
-			}
+                    PNode node(value["datum"].get<size_t>());
+                    for (auto& b : value["branches"].items())
+                    {
+                        node.branches.push_back(b.value().get<size_t>());
+                    }
+                    tree->push_back(node);
+                }
+            }
+            else
+            {
+                return false;
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		static void Serialise(std::string jsonpath, std::vector<bv::PNode>* nodes)
-		{
-			nlohmann::json json;
-			json[u8"nodes"] = {};
-			for (bv::PNode& n : *nodes)
-			{
-				json[u8"nodes"].push_back({
-					{ u8"branches", n.branches },
-					{ u8"datum", n.datum },
-					});
-			}
+        static void Serialise(std::string jsonpath, std::vector<bv::PNode>* nodes)
+        {
+            nlohmann::json json;
+            json[u8"nodes"] = {};
+            for (bv::PNode& n : *nodes)
+            {
+                json[u8"nodes"].push_back({
+                    { u8"branches", n.branches },
+                    { u8"datum", n.datum },
+                    });
+            }
 
-			std::ofstream o(jsonpath, std::ios::binary);
-			o << std::setw(4) << json << std::endl;
-			o.close();
-		}
-	};
+            std::ofstream o(jsonpath, std::ios::binary);
+            o << std::setw(4) << json << std::endl;
+            o.close();
+        }
+    };
 }
--- basic-v/source/parser.cpp
+++ basic-v/source/parser.cpp
@@ -66,7 +66,7 @@ int main(int argc, char* argv[])
 		fileout = path.u8string();
 	}
 
-	bv::PNode::Serialise(fileout, &nodes);
+    bv::PNode::Serialise(fileout, &nodes);
 
 	return 0;
 }
